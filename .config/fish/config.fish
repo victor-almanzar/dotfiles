0dcad24f6a45452e9a0f72689f8410590a64f48f
Victor Almanzar <105403994+victor-almanzar@users.noreply.github.com>
2025-02-22 22:56:17 -0500

    update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,32 +1,12 @@
 if status is-interactive
-    eval (/opt/homebrew/bin/brew shellenv)
-    source (/opt/homebrew/bin/starship init fish --print-full-init | psub)
+    # Commands to run in interactive sessions can go here
+    set -g fish_greeting
     uv generate-shell-completion fish | source
     uvx --generate-shell-completion fish | source
-    # Commands to run in interactive sessions can go here
-    abbr vim "nvim"
+    abbr vi nvim
+    abbr vim nvim
     abbr cat "bat --style=plain"
-    set -g fish_greeting
-
-     # Set fzf configuration file
-    set -gx FZF_DEFAULT_OPTS_FILE ~/.config/fzf.conf
-
-    source (/opt/homebrew/bin/fzf --fish | psub)
-
-    # Unbind the default fzf keys
-    bind -e \ct  # Unbind Ctrl+T
-    bind -e \cr  # Unbind Ctrl+R
-    bind -e \ec  # Unbind Alt+C
-    # Then override the bindings
-    bind -k nul fzf-file-widget  # Ctrl+Space
-    bind \e/ fzf-history-widget  # Alt+/
-    bind \e. fzf-cd-widget  # Alt+.
-
-    # uv
-    fish_add_path "/Users/valman/.local/bin"
-    # go
-    fish_add_path "/Users/valman/go/bin"
 end
 
-# vim: set ft=fish ts=4 sw=4 et: vim: set ft=fish ts=4 sw=4 et:
+# vim: set ft=fish ts=4 sw=4 et:
 
