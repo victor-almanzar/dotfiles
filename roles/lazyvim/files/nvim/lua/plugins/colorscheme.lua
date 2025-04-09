b4f88bf934a17db3616a04b8533d4671c9d48031
Victor Almanzar <105403994+victor-almanzar@users.noreply.github.com>
2025-04-09 15:01:05 -0400

    add jellybeans to nvim theme

--- roles/lazyvim/files/nvim/lua/plugins/colorscheme.lua
+++ roles/lazyvim/files/nvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,32 @@
 return {
-  "folke/tokyonight.nvim",
-  lazy = true,
-  opts = {
-    style = "night",
-    transparent = true,
-  },
+	"folke/tokyonight.nvim",
+	lazy = true,
+	opts = {
+		style = "night",
+		transparent = true,
+	},
+	-- Add jellybeans.nvim to your plugins
+	{
+		"wtfox/jellybeans.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {
+			style = "dark", -- or "light" if you prefer
+			transparent = false,
+			italics = true,
+			flat_ui = true,
+			plugins = {
+				all = false,
+				auto = true, -- automatically apply colors for installed plugins
+			},
+		},
+	},
+
+	-- Set Jellybeans as the default colorscheme
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "jellybeans",
+		},
+	},
 }
