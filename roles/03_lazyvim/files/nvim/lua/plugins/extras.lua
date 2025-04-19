f7b1947e80fb1bd6b76ae76060047a87f023480c
Victor Almanzar <105403994+victor-almanzar@users.noreply.github.com>
2025-04-19 08:40:29 -0400

    include lazyvim extra plugins

---
+++ roles/03_lazyvim/files/nvim/lua/plugins/extras.lua
@@ -0,0 +1,11 @@
+return {
+	{ import = "lazyvim.plugins.extras.lang.python" },
+	{ import = "lazyvim.plugins.extras.lang.ansible" },
+	{ import = "lazyvim.plugins.extras.lang.json" },
+	{ import = "lazyvim.plugins.extras.lang.toml" },
+	{ import = "lazyvim.plugins.extras.lang.yaml" },
+	{ import = "lazyvim.plugins.extras.lang.markdown" },
+	{ import = "lazyvim.plugins.extras.lsp.none-ls" },
+	{ import = "lazyvim.plugins.extras.util.dot" },
+	{ import = "lazyvim.plugins.extras.util.mini-hipatterns" },
+}
